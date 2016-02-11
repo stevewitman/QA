1247ac05aa9b9d8cc9a598dd0817a401e8ee26db
Steve Witman <stevewitman@gmail.com>
2016-02-10 20:48:43 -0700

    used questions controller to handle the new question form post action

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,4 @@ Rails.application.routes.draw do
 
   get '/about' => 'home#about'
 
-  post '/questions' => 'home#temp'
-
-  post '/answers' => 'home#temp'
-  
-  get '/questions/:id' => 'home#question'
-
 end
