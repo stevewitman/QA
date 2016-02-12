eab204eeccaf57b0d5584c7aa99844f33c61c63e
Steve Witman <stevewitman@gmail.com>
2016-02-12 10:08:26 -0700

    skipped section on using sucker_punch since currently incompatible with rails 4

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,6 +5,7 @@ class AnswersController < ApplicationController
 		answer = question.answers.create(answer_params)
 
 		MainMailer.notify_question_author(answer).deliver_now
+
 		session[:current_user_email] = answer_params[:email]
 		redirect_to question
 	end
